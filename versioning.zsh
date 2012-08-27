3a40b57b5a8b80428c68aaab588f8f6a707b260a
Michael Seeberger <michael.seeberger@gmail.com>
2012-08-27 16:13:10 +0200

    Added versioning and a gitignore list

---
+++ versioning.zsh
@@ -0,0 +1,5 @@
+PRODUCT_NAME="Markdown Viewer"
+cd "${PRODUCT_NAME}"
+touch "${PRODUCT_NAME}-Info.plist"
+buildVersion=`git log --oneline | wc -l`
+echo "#define BUILD_VERSION ${buildVersion}" > "InfoPlist.h"
